a7e3dc8ce08ab5e271e29233e0365ff3b9b34c31
Jayben Labrada <jayben.labrada@beeline.com>
2026-05-19 00:53:32 -0400

    Add About Us and Terms of Service pages; add footer links to both layouts

--- CourtBooking/Controllers/HomeController.cs
+++ CourtBooking/Controllers/HomeController.cs
@@ -53,6 +53,8 @@ public class HomeController : Controller
     }
 
     public IActionResult Privacy() => View();
+    public IActionResult About()   => View();
+    public IActionResult Terms()   => View();
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
